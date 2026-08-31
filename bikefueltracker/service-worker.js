f9ea9e9eb3366bda4c42e2835f0e95a30c3022f7
YatVFR <41767344+YatVFR@users.noreply.github.com>
2026-08-31 20:30:33 +0800

    FuelTracker v11 Monthly Analytics
    Merge monthly analytics for Bike and Car FuelTrackers.

--- bikefueltracker/service-worker.js
+++ bikefueltracker/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ridefuel-offline-v10';
+const CACHE_NAME = 'ridefuel-offline-v11';
 const APP_SHELL = [
   './',
   './index.html',
--- carfueltracker/service-worker.js
+++ carfueltracker/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'astinafuel-offline-v10';
+const CACHE_NAME = 'astinafuel-offline-v11';
 const APP_SHELL = [
   './',
   './index.html',
